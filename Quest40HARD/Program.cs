5558dad986ecf7dab4583def002d963a4e913274
Handbrake brake <handbrake@mail.ru>
2022-12-03 18:19:14 +0300

    First try to find corners

---
+++ Quest40HARD/Program.cs
@@ -0,0 +1,24 @@
+﻿//На вход программы подаются три целых положительных числа. Определить , является ли это сторонами треугольника. 
+//Если да, то вывести всю информацию по нему:
+// площадь, периметр, значения углов треугольника в градусах, является ли он прямоугольным, равнобедренным, равносторонним.
+ Console.WriteLine("Введите первое число: ");// Введение числа элементов массива
+ int one = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите второе число: ");
+ int two = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите третье число: ");
+ int three = int.Parse(Console.ReadLine()!);
+
+ if (one< two+three&& two< one+three&& three< one+two)
+ {
+ double perimetr = 0; double area = 0; double firstcorner = 0; double secondcorner = 0;
+ double thirdcorner = 0; string type = "треугольник";
+
+ }
+
+
+// void Triangle()
+// {
+//    double per = one+two+three;
+
+
+// }
